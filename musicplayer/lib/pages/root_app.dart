6d8596ce805a7342e04f6094eae6c66df9fb7eb0
Jaman <jaman.edux@gmail.com>
2021-08-31 00:11:52 +0200

    added FeatherIcons

--- musicplayer/lib/pages/root_app.dart
+++ musicplayer/lib/pages/root_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
 class RootApp extends StatefulWidget {
   const RootApp({ Key? key }) : super(key: key);
@@ -8,6 +9,7 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +18,34 @@ class _RootAppState extends State<RootApp> {
     );
   }
   Widget getFooter() {
+    List items = [
+      FeatherIcons.home,
+      FeatherIcons.book,
+      FeatherIcons.search,
+      FeatherIcons.settings,
+    ];
     return Container(
       height: 80,
-      decoration: BoxDecoration(color: Colors.white),
+      decoration: BoxDecoration(color: Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(items.length, (index) {
+            return IconButton(
+              icon: Icon(
+                items[index],
+                color: Colors.white
+              ),
+              onPressed: () {
+                setState(() {
+                  activeTab = index;
+                });
+              },);
+          })
+        ),
+      ),
     );
   }
 }
