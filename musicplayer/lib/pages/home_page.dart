b4a40cfe824e67bfdffee4fa107c335d745ed336
Jaman <jaman.edux@gmail.com>
2021-09-03 23:45:59 +0200

    added song types

--- musicplayer/lib/pages/home_page.dart
+++ musicplayer/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer/json/songs_json.dart';
+//import 'package:flutter_icons/flutter_icons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -15,30 +17,51 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.black,
       elevation: 0,
-      title: Row(
-        children: [
-          Text('Explore', style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),)
-        ],
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Explore', 
+              style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Icon(Icons.list)
+          ],
+        ),
       ),       
-      ) ,
+      ),
+      body: getBody(),
     );
   }
 
-  Widget getAppBar() {
-    return AppBar(
-      backgroundColor: Colors.black,
-      elevation: 0,
-      title: Row(
+  Widget getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Explore', style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: List.generate(song_type_1.length, (index) {
+                  return Column(
+                    children: [
+                      Text(
+                        song_type_1[index],
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ]
+                  );
+                }),
+              ),
+            ],
+          ),
         ],
       ),
     );
