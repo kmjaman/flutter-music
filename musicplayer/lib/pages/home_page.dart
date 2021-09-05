e447d567b677aa7dd8f94653cfb6490223c5e25b
Jaman <jaman.edux@gmail.com>
2021-09-05 03:42:18 +0200

    added more songs

--- musicplayer/lib/pages/home_page.dart
+++ musicplayer/lib/pages/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int activeMenu1 = 0;
+  int activeMenu2 = 2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,13 +127,131 @@ class _HomePageState extends State<HomePage> {
                                 ),
                               ),
                               SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 180,
+                                child: Text(
+                                  songs[index]['description'],
+                                  maxLines: 1,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                  child: Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 20),
+                  child: Row(
+                    children: List.generate(song_type_2.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 25),
+                        child: GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              activeMenu2 = index;
+                            });
+                          },
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                song_type_2[index],
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: activeMenu2 == index ? Colors.greenAccent : Colors.grey,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,                   
+                              ),
+                              activeMenu2 == index
+                                ? Container(
+                                  width: 10,
+                                  height: 3,
+                                  decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.circular(5))
+                                )
+                              : Container()
+                            ]
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: List.generate(songs.length - 5, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: GestureDetector(
+                          onTap: (){
+                          },
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 180,
+                                height: 180,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(image: AssetImage(songs[index + 5]['img']),
+                                  fit: BoxFit.cover),
+                                  color: Colors.greenAccent,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              SizedBox(
                                 height: 20,
                               ),
-                              Text(songs[index]['description'],
+                              Text(songs[index + 5]['title'],
                               style: TextStyle(
                                 fontSize: 15,
                                 color: Colors.white,
                                 fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 180,
+                                child: Text(
+                                  songs[index + 5]['description'],
+                                  maxLines: 1,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ],
