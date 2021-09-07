665ebe6bcd447e94d993ff13c1ed6d041e034c1f
Jaman <jaman.edux@gmail.com>
2021-09-07 23:43:32 +0200

    added album page

---
+++ musicplayer/lib/pages/album_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlbumPage extends StatefulWidget {
+  const AlbumPage({ Key? key }) : super(key: key);
+
+  @override
+  _AlbumPageState createState() => _AlbumPageState();
+}
+
+class _AlbumPageState extends State<AlbumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
